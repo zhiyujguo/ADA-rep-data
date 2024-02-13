2dc666dcb791e1c9c3232a0374e8d63528ea3a10
Jessica Zhiyu Guo <syriath0263@gmail.com>
2024-02-13 14:57:16 -0500

    Add files via upload

---
+++ cardnp_match.R
@@ -0,0 +1,33 @@
+library(dplyr)
+library(MatchIt)
+library(cobalt)
+library(randChecks)
+
+matched.card <- readRDS("matched.card.RDS")
+ps_npcausal <- readRDS("ps_npcausal.RDS")
+matched.card <- matched.card %>% mutate_at(c("gender","Jan_indic","Feb_indic","Mar_indic",
+                                             "Apr_indic","May_indic","Jun_indic","Jul_indic",
+                                             "Aug_indic","Sep_indic","Oct_indic","Nov_indic",
+                                             "Dec_indic" ), as.factor)
+
+f <- as.formula("gender ~ X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8 + X9 + X10 + X11 + 
+                  X12 + X13 + X14 + X15 + X16 + X17 + X18 + X19 + WPS + WC + 
+                  Sixltr + Dic + anger + anticipation + disgust + fear + joy + 
+                  negative + positive + sadness + surprise + trust + AllPunc + 
+                  Period + Comma + Colon + SemiC + QMark + Exclam + Dash + 
+                  Quote + Apostro + Parenth + OtherP + genin + huliu + LMC + 
+                  LSD + NRC + SWS + pleasure + arousal + dominance + LSDdouble + 
+                  Flesch + Dale.Chall + Dickes.Steiwer + Traenkle.Bailer + 
+                  Jan_indic + Feb_indic + Mar_indic + Apr_indic + May_indic + 
+                  Jun_indic + Jul_indic + Aug_indic + Sep_indic + Oct_indic + 
+                  Nov_indic + Dec_indic")
+cardnp <- matchit(f, data = matched.card[, !(names(matched.card) %in% c("weights"))],
+                  distance = ps_npcausal, method = "nearest", ratio = 1,
+                   caliper = 0.1, std.caliper = T)
+
+matched.cardnp = match.data(cardnp)
+saveRDS(matched.cardnp, file = "matched.cardnp.RDS")
+cardnp_cb <- abs(getStandardizedCovMeanDiffs(matched.cardnp[-c(46,61:77)],matched.cardnp$gender))
+
+mean(cardnp_cb)
+max(cardnp_cb)
--- glm2_match.R
+++ glm2_match.R
@@ -3,16 +3,25 @@ library(MatchIt)
 library(cobalt)
 library(randChecks)
 
+f <- as.formula("gender ~ X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8 + X9 + X10 + X11 + 
+                  X12 + X13 + X14 + X15 + X16 + X17 + X18 + X19 + WPS + WC + 
+                  Sixltr + Dic + anger + anticipation + disgust + fear + joy + 
+                  negative + positive + sadness + surprise + trust + AllPunc + 
+                  Period + Comma + Colon + SemiC + QMark + Exclam + Dash + 
+                  Quote + Apostro + Parenth + OtherP + genin + huliu + LMC + 
+                  LSD + NRC + SWS + pleasure + arousal + dominance + LSDdouble + 
+                  Flesch + Dale.Chall + Dickes.Steiwer + Traenkle.Bailer + 
+                  Jan_indic + Feb_indic + Mar_indic + Apr_indic + May_indic + 
+                  Jun_indic + Jul_indic + Aug_indic + Sep_indic + Oct_indic + 
+                  Nov_indic + Dec_indic")
 cov_15 <-read.csv("cov_15.csv",header = T, sep = ",")
-n <- colnames(cov_15[ ,!(colnames(cov_15) %in% c("ups", "num_comments"))])
-f <- as.formula(paste("gender ~", paste(n[!n %in% "gender"], collapse = " + ")))
-
-glm2 <- matchit(f, data = cov_15,distance = "glm",method = "nearest",ratio = 1,
-                  caliper = 0.1, std.caliper = T)
-cobalt::love.plot(glm2)
-matched.glm2 = match.data(glm2)
+cov_15 <- mutate_all(cov_15, function(x) as.numeric(as.character(x)))
+glm2 <- matchit(formula = f, data = cov_15,
+                distance = "glm",link = "logit",
+                discard = "both",method = "nearest",ratio = 1,
+                caliper = 0.1, std.caliper = T)
+matched.glm2<- match.data(glm2)
 saveRDS(matched.glm2, file = "matched.glm2.RDS")
-glm2cb <- abs(getStandardizedCovMeanDiffs(matched.glm2[-c(47,62:78)],matched.glm2$gender))
-
+glm2cb <-abs(getStandardizedCovMeanDiffs(matched.glm2[-c(47,62:78)],matched.glm2$gender))
 mean(glm2cb)
 max(glm2cb)
